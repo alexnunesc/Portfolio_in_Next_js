b1d38252017557a91a3227b613a22a3070dfb705
alexnunesc <alexnunescdev@gmail.com>
2023-07-19 16:22:08 -0300

    ajustes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,29 @@
-import styles from './Footer.module.scss'
+import { ThemeContext } from '@/hooks/ThemeProvider';
+import styles from './Footer.module.scss';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import Link from 'next/link';
+import { useContext } from 'react';
 
 export default function Footer() {
+  const { theme, toggleTheme, buildWhatsAppLink } = useContext(ThemeContext) as any;
   return (
     <footer className={styles.mainFooter}>
-      <p>Footer</p>
-    </footer>
+      <span>Copyright &copy; 2023 ANC </span>
+
+      <Link className={ styles.icons } href='https://github.com/alexnunesc' target="_blank" rel="noopener noreferrer" >
+        <GitHubIcon />
+      </Link>
+
+      <Link className={ styles.icons } href='https://www.linkedin.com/in/alexnunesc/' target="_blank" rel="noopener noreferrer" >
+        <LinkedInIcon />
+      </Link>
+
+      <Link className={ styles.icons } href={buildWhatsAppLink()} target="_blank" rel="noopener noreferrer">
+        <WhatsAppIcon />
+      </Link>
+  </footer>
   )
 }
--- src/components/projects/Projetos.tsx
+++ src/components/projects/Projetos.tsx
@@ -1,22 +1,52 @@
-// import Head from "next/head";
+'use client';
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 import Footer from "../footer/Footer";
 import styles from "./Projetos.module.scss";
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export default function Projetos() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Duração da animação em milissegundos
+      offset: 100, // Deslocamento em pixels antes da animação ser acionada
+      once: false,
+
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+      
+    
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      delay: 0, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    
+    });
+  }, []);
+
   return (
     <main id="Projetos" className={ styles.mainProjects }>
       {/* <h1>Projetos</h1> */}
       <Link href="#inicio" className={ styles.voltarTop }>Volta ao topo ^ </Link>
       <section className={ styles.sectionProjects }>
-        <div className={ styles.imgs }>
+        <div className={ styles.imgs } data-aos="fade-right">
           <Link className={ styles.imgs } href="https://imusic-mauve.vercel.app/search" target="_blank" rel="noopener noreferrer">
             <Image src="/1.png" alt="projeto um" width={800} height={0} />
             <img src="https://skillicons.dev/icons?i=github,react,js,scss" />
           </Link>
         </div>
-        <div className={ styles.imgs }>
+        <div className={ styles.imgs } data-aos="fade-left">
           <Link className={ styles.imgs } href="https://formstrybe.netlify.app/" target="_blank" rel="noopener noreferrer">
             <Image src="/2.png" alt="projeto dois" width={800} height={0} />
             <img src="https://skillicons.dev/icons?i=github,html,css,js" />
